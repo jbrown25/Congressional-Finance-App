8f54c3fc7581398a344a23b8d672818c9684204d
Justin Brown <justinbrown@Justins-MacBook-Pro.local>
2018-04-12 15:51:09 -0700

    added content section next to graph in top industries

--- src/components/top_industries.js
+++ src/components/top_industries.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import StackedBarChart from './stacked_bar_chart';
-import {sortByTotal} from '../utils/index';
+import {sortByTotal, formatDollars} from '../utils/index';
 
 export default class TopIndustries extends Component {
 
@@ -30,11 +30,24 @@ export default class TopIndustries extends Component {
 		const individuals_data = this.getIndividualsData(industries);
 		const pacs_data = this.getPacsData(industries);
 
+		//get top industry
+		const top_industry_name = industry_names[industry_names.length - 1];
+		const top_industry_individuals = individuals_data[individuals_data.length - 1];
+		const top_industry_pacs = pacs_data[pacs_data.length - 1];
+
+		//content for section next to graph
+		const industry_content = `These are the top ten industries from which this campaign committee receives contributions.  
+		Contributions are given by individuals and PACs associated with these industries.
+		PACs, or political action committees, are organizations that collect and spend money in support of political candidates.  
+		Individuals are generally limited to $2,700 per federal candidate per election, while PACs can contribute up to $5,000.  
+		For example, the industry from which this campaign committe receives the most contributions from is ${top_industry_name}.  
+		They received ${formatDollars(top_industry_individuals)} from individuals and ${formatDollars(top_industry_pacs)} from PACs, for a total of ${formatDollars(top_industry_pacs + top_industry_individuals)}`;
+
 		return (
 			<div className='member_topic_container'>
 				<div className='member_topic_content_column'>
 					<div className='member_topic_content'>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam et augue lorem. Aenean sit amet tincidunt leo. Aenean magna est, pharetra eu blandit at, rhoncus a libero. Morbi luctus ante sed ultricies finibus. Nullam mattis est ac dolor viverra, vel bibendum urna eleifend. Aliquam faucibus rhoncus lorem egestas mattis. Mauris tristique scelerisque massa vitae aliquam. Pellentesque ullamcorper vitae est quis fermentum.</p>
+						<p>{industry_content}</p>
 					</div>
 				</div>
 				<div className='member_topic_chart_column'>
