26f0b6e286122f23a9066be2c65adc0600e57c29
Justin Brown <justinbrown@Justins-MacBook-Pro.local>
2018-04-26 10:30:22 -0700

    changed body to functional component

--- src/components/body.js
+++ src/components/body.js
@@ -1,16 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Member from '../containers/member';
 import FrontPage from '../containers/front_page';
 
-export default class Body extends Component {
+const Body = () => {
+	return (
+		<Switch>
+			<Route path='/members/:crp_id' component={Member} />
+			<Route path='/' component={FrontPage} />
+		</Switch>
+	);
+};
 
-	render(){
-		return (
-			<Switch>
-				<Route path='/members/:crp_id' component={Member} />
-				<Route path='/' component={FrontPage} />
-			</Switch>
-		);
-	}
-}
+export default Body;
